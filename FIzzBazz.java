8193a468b2f49d5bc3f41dae7beb9768e461ef23
OkaGouhei <g.oka1975@gmail.com>
2019-02-10 09:42:35 +0900

    FirstCommit

---
+++ FIzzBazz.java
@@ -0,0 +1,12 @@
+import java.io.Console;
+
+public class Test1{
+     public static void main(String[] args) {
+        // int a = 1;
+        int i = 4;
+        if ( i++ ==5 || i++ < 5 ) {
+        System.out.println("表示");
+        }
+        System.out.println(i);
+      }
+}
